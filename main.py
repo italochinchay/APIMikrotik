04d7f73037bb8be38f30b578550ee731969d2555
ItaloCh <italo.chinchay@udep.edu.pe>
2023-03-10 10:45:44 -0500

    Segunda copia

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import requests
 import json
 import conf
 
-print("hola Clase")
-a = 0
-b = 1
+url="https://192.168.5.67"
+recurso_api="/rest/ip/address"
+
+repuesta = requests.get(url+recurso_api,auth=(conf.user,conf.clave),verify=False)
+print(repuesta.json())
+
